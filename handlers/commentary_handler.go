9a25098e6fb3865f6596917a9c9b5c9ce9178ddc
김성민 <98kimsungmin@naver.com>
2025-12-23 21:56:06 +0900

    Add AGVMgr global variable to handlers package
    - Added AGVMgr *AGVManager global variable
    - Follows the same pattern as CommentarySvc
    - Initialized in main.go line 169

--- handlers/commentary_handler.go
+++ handlers/commentary_handler.go
@@ -7,6 +7,9 @@ import (
 // CommentarySvc - 자동 중계 서비스 (main.go에서 초기화)
 var CommentarySvc *services.CommentaryService
 
+// AGVMgr - AGV 관리자 (main.go에서 초기화)
+var AGVMgr *AGVManager
+
 // TriggerCommentary - 외부에서 해설 이벤트 트리거
 func TriggerCommentary(eventType string, data map[string]interface{}) {
 	if CommentarySvc != nil {
